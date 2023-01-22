02e5470e20f620e2ca02b119f2c22cd6b97e8df6
Vladislav Gordeev <gordeevladislav@gmail.com>
2023-01-22 18:17:46 +0400

    feat: add restrictions to user field

--- Hirely.API/Models/User/CreateUserRequest.cs
+++ Hirely.API/Models/User/CreateUserRequest.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Hirely.API.Models.User
 {
   public class CreateUserRequest
   {
+    [Required]
     public string Username { get; set; }
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
     public string ProfileImageUrl { get; set; }
     public string Password { get; set; }
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
   }
 }
